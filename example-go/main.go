d1d00501d077d9382128b404657b235f2edb12a6
Moritz Dorn <domo1014@h-ka.de>
2022-03-25 17:51:04 +0100

    html und makefile für minimal Beispiel

--- example-go/main.go
+++ example-go/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"syscall/js"
+)
 
 func main() {
 	fmt.Println("hello Go in WebAssembly")
+	document := js.Global().Get("document")
+	p := document.Call("createElement", "p")
+	p.Set("innerHTML", "hello Go in WASM")
+	p.Set("className", "block")
+
+	styles := document.Call("createElement", "style")
+	styles.Set("innerHTML", `
+		.block {
+			border: 1px solid black; color: white; background: black;
+		}
+	`)
+
+	document.Get("head").Call("appendChild", styles)
+	document.Get("body").Call("appendChild", p)
+
 }
