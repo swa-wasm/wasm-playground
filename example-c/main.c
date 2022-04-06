07db09c6d89e636437f9cd6e838c449d019b1a71
Lukas Panni <lukas.panni@outlook.de>
2022-04-06 15:55:53 +0200

    Add wasm-js interop example

--- example-c/main.c
+++ example-c/main.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <emscripten/emscripten.h>
 
-int fib(int n)
+EMSCRIPTEN_KEEPALIVE int fib(int n)
 {
     if (n < 2)
         return 1;
     return fib(n - 1) + fib(n - 2);
 }
 
-int fibI(int n)
+EMSCRIPTEN_KEEPALIVE int fibI(int n)
 {
     if (n == 0)
         return 1;
@@ -20,27 +21,4 @@ int fibI(int n)
         fibs[i] = fibs[i - 1] + fibs[i - 2];
     }
     return fibs[n];
-}
-
-int main()
-{
-    int array[5] = {0, 1, 2, 3, 4};
-    for (int i = 0; i < 5; i++)
-    {
-        printf("Element[%d] = %d\n", i, array[i]);
-    }
-
-    int *dynamic = malloc(1024);
-    printf("malloc allocated\n");
-    int n = 10;
-    for (int i = 0; i < n; i++)
-    {
-        *(dynamic + i) = fib(i);
-        printf("fib(%d) = %d\n", i, *(dynamic + i));
-    }
-
-    printf("fibI(%d) = %d\n", 45, fibI(45));
-
-    free(dynamic);
-    return 0;
 }
